6856a01b4c1772550c3f5acdd983452671a44176
Matheus <matheustorresdev@gmail.com>
2023-11-14 20:14:52 -0300

    finalizando os controllers

--- src/routes/pessoasRoute.js
+++ src/routes/pessoasRoute.js
@@ -9,4 +9,9 @@ router.post("/pessoas", PessoaController.criaPessoa);
 router.put("/pessoas/:id", PessoaController.atualizaPessoa);
 router.delete("/pessoas/:id", PessoaController.apagaPessoa);
 
+router.get('/pessoas/:estudante/matricula/:matriculaId', PessoaController.pegaUmaMatricula)
+router.post('/pessoas/:estudanteId/matricula', PessoaController.criaMatricula)
+router.put('/pessoas/:estudante/matricula/:matriculaId', PessoaController.atualizaMatricula)
+router.delete('pessoas/:estudanteId/matricula/:matriculaId', PessoaController.apagaMatricula)
+
 module.exports = router;
