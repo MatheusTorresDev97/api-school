b52e05602751b335a52ff411132ce3bb59097b22
Matheus <matheustorresdev@gmail.com>
2023-11-16 18:14:43 -0300

    usando mixins

--- src/routes/pessoasRoute.js
+++ src/routes/pessoasRoute.js
@@ -7,15 +7,18 @@ router
 .get("/pessoas", PessoaController.pegaTodasAsPessoas)
 .get("/pessoas/ativo", PessoaController.pegaPessoasAtivas)
 .get("/pessoas/:id", PessoaController.pegaUmaPessoa)
-.post("/pessoas", PessoaController.criaPessoa)
-.put("/pessoas/:id", PessoaController.atualizaPessoa)
-.delete("/pessoas/:id", PessoaController.apagaPessoa)
-
 .get('/pessoas/:estudante/matricula/:matriculaId', PessoaController.pegaUmaMatricula)
+.get('/pessoas/:estudanteId/matricula', PessoaController.pegaMatriculas)
+
+.post("/pessoas", PessoaController.criaPessoa)
 .post('/pessoas/:estudanteId/matricula', PessoaController.criaMatricula)
 .post('/pessoas/:id/restaura', PessoaController.restauraPessoa)
 .post('/pessoas/:estudanteId/matricula/:matriculaId/restaura', PessoaController.restauraMatricula)
+
+.put("/pessoas/:id", PessoaController.atualizaPessoa)
 .put('/pessoas/:estudante/matricula/:matriculaId', PessoaController.atualizaMatricula)
+
+.delete("/pessoas/:id", PessoaController.apagaPessoa)
 .delete('pessoas/:estudanteId/matricula/:matriculaId', PessoaController.apagaMatricula)
 
 module.exports = router;
