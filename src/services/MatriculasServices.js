5533eabeb99ba8c1320fd81cf4c0abe01d89c319
ROBERTO MATHEUS LIMA TORRES <07121450526@pmse.gov>
2023-11-17 12:12:33 -0300

    criado todos os services

---
+++ src/services/MatriculasServices.js
@@ -0,0 +1,11 @@
+const Services = require('./Services')
+
+class MatriculasServices extends Services { 
+    constructor() {
+        super('Turmas')
+    }
+    //métodos específicos do controlador de Pessoas
+
+}
+
+module.exports = MatriculasServices
